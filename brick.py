5054af8c983b854d0b7153a0ba86849f0d633509
AFishyOcean <dljhazelh@gmail.com>
2022-01-12 13:46:08 -0500

    worked on breakout

--- brick.py
+++ brick.py
@@ -2,21 +2,20 @@ import pygame
 
 class Brick(pygame.sprite.Sprite):
 
-    def __init__(self, width, height, color):
+    def __init__(self, width, height, colour):
         # initialize sprite super class
         super().__init__()
 
         # finish setting the class variables to the parameters
-        self.width =
+        self.width = width
+        self.height = height
+        self.colour = colour
 
         # Create a surface with the correct height and width
         self.image = pygame.Surface((self.width, self.height))
 
         # Get the rect coordinates
-        self.rect
+        self.rect = self.image.get_rect()
 
         # Fill the surface with the correct color
-
-
-
-
+        self.image.fill(self.colour)
